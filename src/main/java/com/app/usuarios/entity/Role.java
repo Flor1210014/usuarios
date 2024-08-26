d537d367a8942b3bcb46144a0e6d0f6793066adc
Flor Edith Arriaga Espinosa <floredith1210014@gmail.com>
2024-08-25 20:51:52 -0600

    configuracion inicial

---
+++ src/main/java/com/app/usuarios/entity/Role.java
@@ -0,0 +1,6 @@
+package com.app.usuarios.entity;
+
+public enum Role {
+    ADMIN,
+    USER  
+}
--- src/main/java/com/app/usuarios/entity/Usuario.java
+++ src/main/java/com/app/usuarios/entity/Usuario.java
@@ -1,23 +1,46 @@
 package com.app.usuarios.entity;
 
 import java.sql.Date;
+import java.util.Collection;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.AssertFalse.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Table(name = "usuarios")
-public class Usuario {
-	private static final long serialVersionUID = 1L;
+@Table(name = "usuarios", uniqueConstraints = {@UniqueConstraint(columnNames = {"login"})})
+public class Usuario implements UserDetails {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@Column(name = "login",  nullable = false, length = 20)
 	private String login;
+	
+	@Column(name = "username",  nullable = false, length = 20)
+	private String username;
 	
 	@Column(name = "password", nullable = false, length = 30)
 	private String password;
@@ -35,7 +58,6 @@ public class Usuario {
 	@CreatedDate
 	private Date fechaalta;
 	
-	@Column(name="fechabaja")
 	private Date fechabaja;
 	
 	@Column(name="status", length = 1)
@@ -49,10 +71,8 @@ public class Usuario {
 	@Column(name="fecharevocado")
 	private Date fecharevocado;
 	
-	@Column(name="fecha_vigencia")
 	private Date fecha_vigencia;
 	
-	@Column(name="no_acceso")
 	private int no_acceso;
 	
 	@Column(name = "apellido_paterno", length = 50)
@@ -61,138 +81,34 @@ public class Usuario {
 	@Column(name = "apellido_materno", length = 50)
 	private String apellido_materno;
 	
-	@Column(name="area")
 	private int area;
 	
 	@Column(name="fechamodificacion")
 	private Date fechamodificacion;
-
-	public String getLogin() {
-		return login;
-	}
-
-	public void setLogin(String login) {
-		this.login = login;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public float getClient() {
-		return client;
-	}
-
-	public void setClient(float client) {
-		this.client = client;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public Date getFechaalta() {
-		return fechaalta;
-	}
-
-	public void setFechaalta(Date fechaalta) {
-		this.fechaalta = fechaalta;
-	}
-
-	public Date getFechabaja() {
-		return fechabaja;
-	}
-
-	public void setFechabaja(Date fechabaja) {
-		this.fechabaja = fechabaja;
-	}
-
-	public char getStatus() {
-		return status;
-	}
-
-	public void setStatus(char status) {
-		this.status = status;
-	}
-
-	public float getIntentos() {
-		return intentos;
-	}
-
-	public void setIntentos(float intentos) {
-		this.intentos = intentos;
-	}
-
-	public Date getFecharevocado() {
-		return fecharevocado;
-	}
-
-	public void setFecharevocado(Date fecharevocado) {
-		this.fecharevocado = fecharevocado;
-	}
-
-	public Date getFecha_vigencia() {
-		return fecha_vigencia;
-	}
-
-	public void setFecha_vigencia(Date fecha_vigencia) {
-		this.fecha_vigencia = fecha_vigencia;
-	}
-
-	public int getNo_acceso() {
-		return no_acceso;
-	}
-
-	public void setNo_acceso(int no_acceso) {
-		this.no_acceso = no_acceso;
-	}
-
-	public String getApellido_paterno() {
-		return apellido_paterno;
-	}
-
-	public void setApellido_paterno(String apellido_paterno) {
-		this.apellido_paterno = apellido_paterno;
-	}
-
-	public String getApellido_materno() {
-		return apellido_materno;
-	}
-
-	public void setApellido_materno(String apellido_materno) {
-		this.apellido_materno = apellido_materno;
-	}
-
-	public int getArea() {
-		return area;
-	}
-
-	public void setArea(int area) {
-		this.area = area;
-	}
-
-	public Date getFechamodificacion() {
-		return fechamodificacion;
-	}
-
-	public void setFechamodificacion(Date fechamodificacion) {
-		this.fechamodificacion = fechamodificacion;
-	}
+	
+	@Enumerated(EnumType.STRING) 
+    Role role;
+	
+	
+	@Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+      return List.of(new SimpleGrantedAuthority((role.name())));
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+       return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+       return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 	
 }
