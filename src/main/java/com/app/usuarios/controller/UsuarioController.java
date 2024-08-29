f51e1a06e1e45f0e5aeb720382f868320ab6b477
Flor Edith <floredith1210014@gmail.com>
2024-08-28 18:03:00 -0600

    listar todos los ususrios

--- src/main/java/com/app/usuarios/controller/UsuarioController.java
+++ src/main/java/com/app/usuarios/controller/UsuarioController.java
@@ -1,8 +1,19 @@
 package com.app.usuarios.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.app.usuarios.entity.Usuario;
+import com.app.usuarios.service.UsuarioService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,10 +21,25 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class UsuarioController {
+    @Autowired
+	private UsuarioService usuarioService;
     
     @PostMapping(value = "demo")
     public String welcome()
     {
         return "Welcome from secure endpoint";
     }
+    
+    @GetMapping(value="/usuarios")
+	public ResponseEntity<Object> get(){ 
+		java.util.Map<String, Object> map = new HashMap<String, Object>();
+		try {
+			List<Usuario> list  = usuarioService.findAll();
+			return new ResponseEntity<Object>(list,HttpStatus.OK);
+		} 
+		catch (Exception e) {
+			map.put("message", e.getMessage());
+			return new ResponseEntity<>( map, HttpStatus.INTERNAL_SERVER_ERROR);
+		} 
+ 	}
 }
--- src/main/java/com/app/usuarios/service/UsuarioServiceImpl.java
+++ src/main/java/com/app/usuarios/service/UsuarioServiceImpl.java
@@ -1,5 +1,49 @@
 package com.app.usuarios.service;
 
-public class UsuarioServiceImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.app.usuarios.entity.Usuario;
+import com.app.usuarios.repository.UsuarioDao;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService{
+
+    @Autowired
+	private UsuarioDao usuarioDao;
+	
+	
+
+	@Override
+	public List<Usuario> findAll() {
+		return (List<Usuario>) usuarioDao.findAll();
+	}
+
+	@Override
+	public Usuario save(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Usuario update(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Usuario findByLogin(String login) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Usuario usuario) {
+		// TODO Auto-generated method stub
+		
+	}
 }
